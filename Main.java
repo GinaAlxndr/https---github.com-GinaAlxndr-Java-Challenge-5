5291bb5102ab48a4349cda748b3da6073978d2cf
gina khalisa <khalisa.gina18@gmail.com>
2021-10-22 13:47:39 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,46 @@
+class Main {
+    public static void main(String[] args) {
+      Heroes hero = new Heroes();
+  
+      hero.setProfile("Gina", 20, 165);
+      hero.profile();
+      
+  
+      // jalankan aktivitas disini...
+  
+      // contoh:
+      hero.jalan();
+      hero.jalan();
+      hero.jalan();
+      hero.jalan();
+      hero.lari();
+      hero.makan();
+      hero.makan();
+      hero.makan();
+      hero.minum();
+      hero.minum();
+      hero.minum();
+      hero.minum();
+      hero.minum();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.duduk();
+      hero.duduk();
+      hero.duduk();
+  
+      // challenge:
+      /*
+     
+      Group 5: 
+     
+      */
+        
+  
+      // tampilkan status terkini...
+      hero.profile();
+      
+    }
+  }
